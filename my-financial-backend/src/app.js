9a4638e0763ac627d3ffc1887afd8c684916334e
thorongil05 <officialantonio@outlook.com>
2025-12-26 20:51:46 +0100

    fix: temporary fix problem with CORS

--- my-financial-backend/src/app.js
+++ my-financial-backend/src/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", routes);
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 console.log("App loaded");
 
