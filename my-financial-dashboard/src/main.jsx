8cc26326254b1be6fccc1c4bdfc27b7887c07ad5
thorongil05 <a.acquavia@studenti.unipi.com>
2025-12-29 17:04:24 +0100

    feat: remove useless stuff from main.js

--- my-financial-dashboard/src/main.jsx
+++ my-financial-dashboard/src/main.jsx
@@ -1,14 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter } from "react-router";
 
 const root = document.getElementById("root");
 
 createRoot(root).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<App />}></Route>
-    </Routes>
+    <App></App>
   </BrowserRouter>
 );
