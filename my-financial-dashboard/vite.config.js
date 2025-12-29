fec2d1994644d5c92d16258642062be7eaa9b964
thorongil05 <officialantonio@outlook.com>
2025-12-29 10:29:32 +0100

    build: add start command and add ports for preview and server (dev mode)

--- my-financial-dashboard/vite.config.js
+++ my-financial-dashboard/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    port: 5713,
+  },
+  server: {
+    port: 5713,
+  },
   plugins: [react(), tailwindcss()],
-})
+});
