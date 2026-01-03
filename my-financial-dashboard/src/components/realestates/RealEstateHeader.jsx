d695a670e94781efab052a2295d934acc40a4612
thorongil05 <a.acquavia@studenti.unipi.com>
2026-01-03 11:09:18 +0100

    feat: add grid in real estate header

--- my-financial-dashboard/src/components/realestates/RealEstateHeader.jsx
+++ my-financial-dashboard/src/components/realestates/RealEstateHeader.jsx
@@ -1,5 +1,9 @@
 import PropTypes from "prop-types";
 import { ScatterChart } from "@mui/x-charts/ScatterChart";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
 
 function RealEstateHeader({ realEstatesInfo }) {
   let series = realEstatesInfo.map((element) => ({
@@ -8,15 +12,29 @@ function RealEstateHeader({ realEstatesInfo }) {
     y: element.area,
   }));
   return (
-    <ScatterChart
-      height={200}
-      series={[
-        {
-          label: "Series A",
-          data: series,
-        },
-      ]}
-    />
+    <Grid container spacing={2}>
+      <Grid size={4}>
+        <Card variant="outlined">
+          <CardHeader title="Relazione Costo/Metratura"></CardHeader>
+          <CardContent>
+            <ScatterChart
+              height={200}
+              series={[
+                {
+                  label: "Series A",
+                  data: series,
+                },
+              ]}
+            />
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid size={4}>
+        <Card variant="outlined">
+          <CardHeader title="Statistiche Varie"></CardHeader>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
