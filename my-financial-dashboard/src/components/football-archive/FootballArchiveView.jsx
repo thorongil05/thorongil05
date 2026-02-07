9458d7b787838ec3b7419a8ee5680374d535f9d2
thorongil05 <a.acquavia@studenti.unipi.com>
2026-02-07 06:31:32 +0100

    feat: extract a new component for team dialog

--- my-financial-dashboard/src/components/football-archive/FootballArchiveView.jsx
+++ my-financial-dashboard/src/components/football-archive/FootballArchiveView.jsx
@@ -1,86 +1,10 @@
 import CompetitionSelector from "./CompetitionSelector";
-import {
-  TextField,
-  Dialog,
-  DialogTitle,
-  IconButton,
-  Stack,
-  Button,
-} from "@mui/material";
+import { IconButton, Stack } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-
-function submit(e, formData, onSubmitAction) {
-  e.preventDefault();
-  const apiUrl = `${import.meta.env.VITE_SERVER_URL}/api/teams/`;
-  console.log("Calling submit on url", apiUrl);
-  let options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  };
-  console.log(JSON.stringify(options));
-  fetch(apiUrl, options)
-    .then((response) => {
-      console.log(response.json());
-      console.log("Success, executing on submit action");
-      onSubmitAction();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
-
-function AddTeamDialog({ onClose, open }) {
-  let [formData, setFormData] = useState({
-    name: "",
-    city: "",
-  });
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
-
-  return (
-    <Dialog onClose={onClose} open={open}>
-      <DialogTitle>Aggiungi squadra al campionato</DialogTitle>
-      <form>
-        <Stack spacing={2} className="m-3">
-          <TextField
-            size="small"
-            label="Nome"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-          ></TextField>
-          <TextField
-            size="small"
-            label="Cittá"
-            name="city"
-            value={formData.city}
-            onChange={handleChange}
-          ></TextField>
-          <Button type="submit" onClick={(e) => submit(e, formData, () => {})}>
-            Submit
-          </Button>
-        </Stack>
-      </form>
-    </Dialog>
-  );
-}
-
-AddTeamDialog.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-};
+import AddTeamDialog from "./AddTeamDialog";
 
 function FootballArchiveView() {
   const [open, setOpen] = useState(false);
