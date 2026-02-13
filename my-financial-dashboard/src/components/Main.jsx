928f5b1c2decf2468d663785b8ece2d64a42681b
thorongil05 <a.acquavia@studenti.unipi.com>
2026-02-13 21:51:31 +0100

    style: add padding

--- my-financial-dashboard/src/components/Main.jsx
+++ my-financial-dashboard/src/components/Main.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router";
 import Navbar from "./core/Navbar";
 import Footer from "./core/Footer";
+import { Container, Box } from "@mui/material";
 
 const layoutStyle = {
   display: "flex",
@@ -8,17 +9,17 @@ const layoutStyle = {
   minHeight: "100vh",
 };
 
-const mainStyle = { flex: 1, padding: "2rem 0 2rem 0" };
-
 function Main() {
   return (
-    <div style={layoutStyle}>
-      <Navbar></Navbar>
-      <div style={mainStyle}>
-        <Outlet></Outlet>
-      </div>
-      <Footer></Footer>
-    </div>
+    <Box sx={layoutStyle}>
+      <Navbar />
+      <Box component="main" sx={{ flex: 1, py: 4 }}>
+        <Container maxWidth="lg">
+          <Outlet />
+        </Container>
+      </Box>
+      <Footer />
+    </Box>
   );
 }
 
