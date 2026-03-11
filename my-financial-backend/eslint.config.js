457beb6f5d5b7624fa939745a2d169840a3c66ef
thorongil05 <a.acquavia@studenti.unipi.com>
2026-03-11 12:28:53 +0100

    feat: add eslint

--- my-financial-backend/eslint.config.js
+++ my-financial-backend/eslint.config.js
@@ -1,32 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import sonarjs from "eslint-plugin-sonarjs";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
   {
-    files: ["**/*.{js,jsx}"],
+    files: ["**/*.js"],
     plugins: {
       sonarjs,
     },
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
       },
     },
     rules: {
+      ...js.configs.recommended.rules,
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
       "sonarjs/cognitive-complexity": ["error", 15],
       complexity: ["error", 10],
